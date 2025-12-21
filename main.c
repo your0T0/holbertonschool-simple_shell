fee7c6e91bac72d541264c90d537550eaf4577a1
manar <mnarzaid247@gmail.com>
2025-12-21 15:20:25 +0000

    finishing the main file

--- main.c
+++ main.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 
 /**
  * main - main function
  *
- * @void: dont receve anything
- *
- * Return: nothing
+ * Return: 0
  */
 int main(void)
 {
@@ -17,7 +16,8 @@ char *line = NULL;
 size_t len = 0;
 char *argv[64];
 int i = 0;
-char *token = strtok(line, " ");
+char *token;
+pid_t pid;
 while (1)
 {
 if (inter)
@@ -27,12 +27,23 @@ if (getline(&line, &len, stdin) == -1)
 line[strcspn(line, "\n")] = '\0';
 if (line[0] == '\0')
 	continue;
+token = strtok(line, " ");
+i = 0;
 while (token != NULL)
 {
 argv[i++] = token;
 token = strtok(NULL, " ");
 }
 argv[i] = NULL;
+pid = fork();
+if (pid == 0)
+{
+execve(argv[0], argv, NULL);
+perror("execve");
+exit(1);
+}
+else if (pid > 0)
+	wait(NULL);
 }
 free(line);
 return (0);
